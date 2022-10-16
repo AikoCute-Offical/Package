854f36432da1c8708f24b8c3f8411ef7c18c80f3
AikoCute <aikocute@icloud.com>
2022-10-16 14:25:09 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,8 @@
 package main
+
+import "github.com/AikoCute-Offical/GoPackage/onedrive"
+
+// DownloadFile download file from onedrive
+func OneDriver(patch string, url string) {
+	onedrive.DownloadFile(patch, url)
+}
