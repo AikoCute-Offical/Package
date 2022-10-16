4a03b48adb20c463f6e5f5bc0246138dc749c4ea
AikoCute <aikocute@icloud.com>
2022-10-16 14:13:20 +0700

    Update Package

---
+++ onedrive/dowload.go
@@ -0,0 +1,17 @@
+package gopackage
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func OneDriver(patch string, url string) {
+	cmd := exec.Command("wget", url+"&download=1", "-O", patch)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
